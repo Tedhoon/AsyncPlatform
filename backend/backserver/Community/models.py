5efa396010b704ed7b8185a1a305c5f3226143d2
Tedhoon <gt0305@likelion.org>
2020-05-09 18:22:19 +0900

    [add] author_name 적용

--- backend/backserver/Community/models.py
+++ backend/backserver/Community/models.py
@@ -1,11 +1,11 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.conf import settings
 
 
 class Community(models.Model):
     title = models.CharField(max_length = 100)
     desc = models.TextField(max_length = 100)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, null=True, related_name="community")
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True, related_name="community")
 
     # def __str__(self):
     #     return self.title
--- backend/backserver/Community/serializers.py
+++ backend/backserver/Community/serializers.py
@@ -2,6 +2,10 @@ from rest_framework import serializers
 from .models import Community
 
 class CommunitySerializer(serializers.ModelSerializer):
+
+    author_name = serializers.ReadOnlyField(source='author.username')
+
     class Meta:
         model = Community
-        fields = '__all__'
+        fields = ('id', 'title', 'desc', 'author_name')
+        
